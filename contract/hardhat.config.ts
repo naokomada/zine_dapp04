457af62da43b7129f70a8eb269bc921a751d0887
madzonbiesbookstore <madzonbiesbookstore@gmail.com>
2025-08-31 17:57:40 +0900

    不要な記載の削除

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -16,22 +16,3 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
-
-
-
-// import "dotenv/config";
-
-// const BASE_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_RPC_URL;
-// const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
-// const config: HardhatUserConfig = {
-//   solidity: "0.8.24",
-//   networks: {
-//     base_sepolia: {
-//       url: BASE_SEPOLIA_RPC_URL,
-//       accounts: [PRIVATE_KEY],
-//     },
-//   },
-// };
-
-// export default config;
